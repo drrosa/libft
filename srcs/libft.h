e9b9a63ef955f52ca6168524eb960523fcd7da98
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-04 21:25:53 -0800

    Add libft.h

---
+++ srcs/libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/04 21:30:00 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <string.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *s);
+size_t	ft_strlen(const char *s);
+int		ft_strcmp(const char *s1, const char *s2);
+
+#endif
