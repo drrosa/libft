c9e6f8b9eca4f83d186245331fe9f82ac921dc2d
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-05 18:41:48 -0800

    Add ft_atoi

---
+++ srcs/ft_atoi.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/05 18:10:42 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/05 18:39:22 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int result;
+	int i;
+	int sign;
+
+	result = 0;
+	i = 0;
+	sign = 1;
+	while ((str[i] < '0' || str[i] > '9') && (str[i] < 'A' ||
+			str[i] > 'Z') && (str[i] < 'a' || str[i] > 'z'))
+	{
+		if (str[i++] == '-')
+		{
+			sign = -1;
+			break;
+		}
+	}	
+	while (str[i])
+	{
+		if (str[i] - '0' < 0 || str[i] - '0' > 9)
+			break;
+		result = result * 10 + str[i++] - '0';
+	}
+	return (result * sign );
+}
--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/04 21:30:00 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/05 18:26:25 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@ void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 size_t	ft_strlen(const char *s);
 int		ft_strcmp(const char *s1, const char *s2);
+int		ft_atoi(const char *str);
 
 #endif
