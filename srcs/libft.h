c62366e4802f3abf62d7442a821d343bd09cd63b
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-06 15:29:41 -0800

    Add ft_strncpy

--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 15:22:49 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/06 15:24:37 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ char	*ft_strcat(char *s1, const char *s2);
 char	*ft_strncat(char *s1, const char *s2, size_t n);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t len);
 
 #endif
