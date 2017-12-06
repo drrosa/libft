58948c1d4a755d1e3036eae9e3a7f5a4946a4034
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-06 15:46:34 -0800

    Add NULL check after using malloc()

--- srcs/ft_strdup.c
+++ srcs/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 22:25:19 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/05 22:26:31 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/06 15:45:29 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,14 @@ char	*ft_strdup(const char *s1)
 	while (s1[size])
 		size++;
 	dst = (char*)malloc(sizeof(*dst) * size + 1);
-	while (s1[i])
+	if (dst != NULL)
 	{
-		dst[i] = s1[i];
-		i++;
+		while (s1[i])
+		{
+			dst[i] = s1[i];
+			i++;
+		}
+		dst[i] = '\0';
 	}
-	dst[i] = '\0';
 	return (dst);
 }
