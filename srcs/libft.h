164c8ad8b0f3817cf23bd0c161968fdd196fd58c
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-06 14:34:10 -0800

    Add ft_strcat

--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 14:14:33 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/06 14:33:55 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,12 @@ int		ft_strcmp(const char *s1, const char *s2);
 int		ft_atoi(const char *str);
 void	ft_putnbr(int n);
 char	*ft_itoa(int n);
-char    *ft_strdup(const char *s1);
+char	*ft_strdup(const char *s1);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+char	*ft_strcat(char *restrict s1, const char *restrict s2);
 
 #endif
