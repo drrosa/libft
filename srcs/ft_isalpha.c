412a592a37ebf43c9ea1c36e3a9e2f86eb483dc0
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-06 13:39:53 -0800

    Add ft_isalpha

--- srcs/ft_isalpha.c
+++ srcs/ft_isalpha.c
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 13:38:57 by drosa-ta         ###   ########.fr       */
+/*   Created: 2017/12/06 13:29:44 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/06 13:35:25 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <string.h>
-
-void	ft_putchar(char c);
-void	ft_putstr(char const *s);
-size_t	ft_strlen(const char *s);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_atoi(const char *str);
-void	ft_putnbr(int n);
-char	*ft_itoa(int n);
-char    *ft_strdup(const char *s1);
-int		ft_isalpha(int c);
-
-#endif
+int	ft_isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
