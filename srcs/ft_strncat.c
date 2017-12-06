ace0db7b2a3969f9e636644b79a31c693e6ddb93
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-06 15:28:24 -0800

    Fix norm

--- srcs/ft_strncat.c
+++ srcs/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/06 15:00:46 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 15:06:15 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/06 15:27:57 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*ft_strncat(char *s1, const char *s2, size_t n)
 {
-	unsigned long i;
-	size_t size;
+	unsigned long	i;
+	size_t			size;
 
 	i = 0;
 	size = 0;
