2efdbee80c15ce5aed5dbf2953c22d422c5dd6de
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-07 12:08:31 -0800

    Add ft_strnstr

---
+++ srcs/ft_strnstr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/06 15:35:39 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/07 12:06:21 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	unsigned long i;
+	unsigned long j;
+	unsigned long size;
+
+	i = 0;
+	j = 0;
+	size = 0;
+	while (little[size])
+		size++;
+	if (size == 0)
+		return ((char *)big);
+	while (big[i])
+	{
+		while (little[j] == big[i + j])
+		{
+			if (j == size - 1)
+				return ((char *)(big + i));
+			j++;
+		}
+		if (i + j + 1 == len)
+			return (NULL);
+		j = 0;
+		i++;
+	}
+	return (NULL);
+}
--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 15:56:25 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/07 11:08:36 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,5 +33,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strstr(const char *big, const char *little);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 #endif
