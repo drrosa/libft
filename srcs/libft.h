628da07713632597edaed9f6e6575d6bf3084af5
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-07 23:19:18 -0800

    Add ft_isascii

--- srcs/libft.h
+++ srcs/libft.h
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 14:44:24 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/07 23:04:42 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/07 23:14:07 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_isalnum(int c);
+int		ft_isascii(int c);
 
 #endif
