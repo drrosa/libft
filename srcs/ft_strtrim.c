66b31c7aca4a2ee194ab3d41054dd166007eef16
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-09 23:45:32 -0800

    Add ft_strim

---
+++ srcs/ft_strtrim.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/09 21:53:25 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/09 23:44:49 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strtrim(char const *s)
+{
+	unsigned long	i;
+	unsigned long	n;
+	unsigned long	j;
+	char			*str;
+
+	if (!s || (str = (char *)malloc(sizeof(char))) == 0)
+		return (NULL);
+	if (*s == '\0')
+		return (str);
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
+		i++;
+	n = ft_strlen(s) - 1;
+	while (s[n] == ' ' || s[n] == '\n' || s[n] == '\t')
+		n--;
+	n = n - i;
+	if (!(str = ft_strnew(n + 1)))
+		return (NULL);
+	j = 0;
+	while (j <= n)
+		str[j++] = s[i++];
+	return (str);
+}
