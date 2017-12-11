65f4b4f50d4416a6da7b0f60168ed7a812f21d90
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-10 22:38:29 -0800

    Fix ft_strtrim when input is all blank space

--- srcs/ft_strtrim.c
+++ srcs/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/09 21:53:25 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/09 23:44:49 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/10 18:34:45 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,15 @@ char	*ft_strtrim(char const *s)
 
 	if (!s || (str = (char *)malloc(sizeof(char))) == 0)
 		return (NULL);
+	str[0] = '\0';
 	if (*s == '\0')
 		return (str);
 	i = 0;
 	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
 		i++;
 	n = ft_strlen(s) - 1;
+	if (i >= n)
+		return (str);
 	while (s[n] == ' ' || s[n] == '\n' || s[n] == '\t')
 		n--;
 	n = n - i;
