864fc87a34115a43bee65f1c12dfe05cec851647
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-12 15:44:07 -0800

    ft_itoa: Fixed handling MIN int.

--- srcs/ft_itoa.c
+++ srcs/ft_itoa.c
@@ -6,44 +6,45 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 19:01:14 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/06 22:39:49 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/12 15:42:31 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include "libft.h"
 
 char	*ft_itoa(int n)
 {
+	char	*result;
+	int		size;
 	int		tmp;
-	int		i;
-	char	*str;
 
+	size = 0;
 	tmp = n;
-	i = 0;
-	if (n <= 0)
-		i = 1;
-	while (tmp != 0)
+	while (tmp)
 	{
 		tmp = tmp / 10;
-		i++;
+		size++;
 	}
-	str = (char *)malloc(sizeof(*str) * (i + 1));
-	if (!str)
+	if (n <= 0)
+		size++;
+	result = (char *)malloc(sizeof(char) * size + 1);
+	if (!result)
 		return (NULL);
-	if (n == '\0' || n == 0)
-		str[0] = '0';
+	result[size] = '\0';
+	if (n == 0)
+		result[0] = '0';
+	if (n == -2147483648)
+		return (ft_strcpy(result, "-2147483648"));
 	if (n < 0)
 	{
-		str[0] = '-';
+		result[0] = '-';
 		n = n * -1;
 	}
-	str[i] = '\0';
-	i--;
-	while (n != 0)
+	while (n)
 	{
-		str[i] = (n % 10) + '0';
+		result[--size] = n % 10 + '0';
 		n = n / 10;
-		i--;
 	}
-	return (str);
+	return (result);
 }
