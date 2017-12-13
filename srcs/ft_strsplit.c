f8aeadd53255bab3354c8894d4eea7803f99c76f
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-12 16:23:38 -0800

    ft_strsplit: Add ft_strcleanup function

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -6,25 +6,25 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 21:42:05 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/10 21:42:08 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/12 16:21:02 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void compress(const char *str, char **ptr, char c)
+void	compress(const char *str, char **ptr, char c)
 {
-    int i;
-    int j;
+	int i;
+	int j;
 	int n;
 
-    i = 0;
-    j = 0;
+	i = 0;
+	j = 0;
 	n = 0;
-    while (str[i] != '\0')
-    {
-		while (str[i+1] && str[i] == c)
+	while (str[i] != '\0')
+	{
+		while (str[i + 1] && str[i] == c)
 		{
 			i++;
 		}
@@ -33,21 +33,21 @@ void compress(const char *str, char **ptr, char c)
 		if (str[i + 1] == c)
 		{
 			*ptr = ft_strnew(n);
-			while(n)
+			while (n)
 			{
-				**ptr++ = (char )str[i - n--];
+				**ptr++ = (char)str[i - n--];
 			}
 			ptr++;
 		}
 		j++;
 		i++;
-    }
+	}
 }
 
 int count(const char *str, char c)
 {
-    unsigned long i;
-    int j;
+	unsigned long i;
+	int j;
 
 	i = 0;
 	j = 0;
@@ -57,7 +57,7 @@ int count(const char *str, char c)
 		{
 			i++;
 		}
-		if(str[i] == c)
+		if (str[i] == c)
 			j++;
 		i++;
 	}
@@ -66,21 +66,48 @@ int count(const char *str, char c)
 	return (j);
 }
 
+char	*ft_strcleanup(char const *str, char c)
+{
+	int i;
+	int j;
+	char *result;
+
+	i = 0;
+	j = 0;
+	result = ft_strnew(ft_strlen(str));
+	while (str[i])
+	{
+		while (str[i] == c)
+			i++;
+		while (str[i] && str[i] != c)
+		{
+			result[j++] = str[i];
+			i++;
+		}
+		while (str[i] == c)
+			i++;
+		if (str[i] && str[i] != c)
+			result[j++] = c;
+	}
+	result[j] = '\0';
+	return (result);
+}
+
 #include <stdio.h>
 
-char	**ft_strsplit(char const *str, char c)
+char **ft_strsplit(char const *str, char c)
 {
-	char	**ptr = NULL;
-	char	*temp;
-	int		size;
+	char **ptr = NULL;
+	char *temp;
+	int size;
 
-	if(!str && !*str)
+	if (!str && !*str)
 		return (NULL);
 
 	size = count(str, c);
-	ptr = (char**)malloc(sizeof(char*) * (size + 2));
-	temp = (char*)malloc(sizeof(char) * (size + 1));
-	if(!ptr || !*ptr || !temp)
+	ptr = (char **)malloc(sizeof(char *) * (size + 2));
+	temp = (char *)malloc(sizeof(char) * (size + 1));
+	if (!ptr || !*ptr || !temp)
 		return (NULL);
 	if (size == 0)
 	{
@@ -89,9 +116,9 @@ char	**ft_strsplit(char const *str, char c)
 		temp[0] = '\0';
 		return (ptr);
 	}
-//	printf("HERE - size -> %d\n", size);
+	//	printf("HERE - size -> %d\n", size);
 	compress(str, ptr, c);
-//	printf("%s\n", temp);
+	//	printf("%s\n", temp);
 	// *ptr = temp;
 	return (ptr);
 }
