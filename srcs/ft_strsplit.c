2b23270401235d6693582331b9d402fc59c30f94
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-14 00:02:00 -0800

    ft_strsplit: Finally works!

--- srcs/ft_strsplit.c
+++ srcs/ft_strsplit.c
@@ -6,75 +6,57 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 21:42:05 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/12 16:21:02 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/14 00:00:40 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-void	compress(const char *str, char **ptr, char c)
+#include <stdio.h>
+void	split(const char *str, char **ptr, char c)
 {
 	int i;
 	int j;
 	int n;
+	int k;
 
 	i = 0;
 	j = 0;
+	k = 0;
 	n = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i + 1] && str[i] == c)
+		if (str[i + 1] == c || !str[i + 1])
 		{
-			i++;
-		}
-		if (str[i] != c)
-			n++;
-		if (str[i + 1] == c)
-		{
-			*ptr = ft_strnew(n);
-			while (n)
+			n = i - k;
+			ptr[j] = ft_strnew(n + 1);
+			k = 0;
+			while (k < n + 1)
 			{
-				**ptr++ = (char)str[i - n--];
+				ptr[j][k] = (char)str[(i - n - 1) + k + 1];
+				k++;
 			}
-			ptr++;
-		}
-		j++;
-		i++;
-	}
-}
-
-int count(const char *str, char c)
-{
-	unsigned long i;
-	int j;
-
-	i = 0;
-	j = 0;
-	while (str[i] != '\0')
-	{
-		while (str[i + 1] && str[i] == c)
-		{
 			i++;
-		}
-		if (str[i] == c)
+			k = i + 1;
 			j++;
+		}
+		ptr[j] = 0;
+
 		i++;
 	}
-	if (i - 1 == ft_strlen(str))
-		j = 0;
-	return (j);
 }
 
-char	*ft_strcleanup(char const *str, char c)
+int		ft_strcleanup(char const *str, char *result, char c)
 {
 	int i;
 	int j;
-	char *result;
+	int wc;
 
 	i = 0;
 	j = 0;
-	result = ft_strnew(ft_strlen(str));
+	wc = 0;
+
 	while (str[i])
 	{
 		while (str[i] == c)
@@ -87,38 +69,46 @@ char	*ft_strcleanup(char const *str, char c)
 		while (str[i] == c)
 			i++;
 		if (str[i] && str[i] != c)
+		{
 			result[j++] = c;
+			wc++;
+		}
 	}
 	result[j] = '\0';
-	return (result);
+	return (wc + 1);
 }
 
-#include <stdio.h>
-
-char **ft_strsplit(char const *str, char c)
+char	**ft_strsplit(char const *str, char c)
 {
-	char **ptr = NULL;
+	char **ptr;
 	char *temp;
 	int size;
-
-	if (!str && !*str)
+	if (!str)
 		return (NULL);
-
-	size = count(str, c);
+	temp = ft_strnew(ft_strlen(str));
+	size = ft_strcleanup(str, temp, c);
 	ptr = (char **)malloc(sizeof(char *) * (size + 2));
-	temp = (char *)malloc(sizeof(char) * (size + 1));
-	if (!ptr || !*ptr || !temp)
+	if (!ptr || !temp)
 		return (NULL);
-	if (size == 0)
+	if (str[0] == '\0' || size == 1)
 	{
-		ptr[0] = temp;
-		ptr[1] = 0;
-		temp[0] = '\0';
+		ptr[0] = 0;
 		return (ptr);
 	}
-	//	printf("HERE - size -> %d\n", size);
-	compress(str, ptr, c);
-	//	printf("%s\n", temp);
-	// *ptr = temp;
+	split(temp, ptr, c);
+	int i;
+	i = 0;
 	return (ptr);
 }
+
+// #include <stdlib.h>
+// int main()
+// {
+// 	char *
+//  src = "      split       this for   me  !       ";
+// 	char **r = ft_strsplit(src, ' ');
+// 	int i;
+// 	i = 0;
+// 	while(r[i])
+// 		printf("%s ", r[i++]);
+// }
