2236b9189c12c6d8dcedde0cb2a7ef9512924608
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-15 12:57:13 -0800

    Add ft_mem* functions

--- srcs/libft.h
+++ srcs/libft.h
@@ -58,5 +58,12 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, const char *s2);
 char	*ft_strtrim(char const *s);
+void    *ft_memset(void *b, int c, size_t len);
+void    ft_bzero(void *s, size_t n);
+void    *ft_memcpy(void *dst, const void *src, size_t n);
+void    *ft_memccpy(void *dst, const void *src, int c, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t n);
+void    *ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
