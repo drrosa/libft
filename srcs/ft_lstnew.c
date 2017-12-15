0ce0fb261d18a9b3d0319bf2889edc6d67a97d2d
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-15 13:17:10 -0800

    Add ft_lstnew

---
+++ srcs/ft_lstnew.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 12:48:33 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/15 12:48:36 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (!node)
+		return (NULL);
+	if (!content)
+	{
+		node->content = NULL;
+		node->content_size = 0;
+	}
+	else
+	{
+		node->content = malloc(sizeof(content_size));
+		if (!node->content)
+		{
+			free(node);
+			return (NULL);
+		}
+		ft_memcpy(node->content, content, content_size);
+		node->content_size = content_size;
+	}
+	node->next = NULL;
+	return (node);
+}
--- srcs/libft.h
+++ srcs/libft.h
@@ -65,5 +65,12 @@ void    *ft_memccpy(void *dst, const void *src, int c, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t n);
 void    *ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+t_list	*ft_lstnew(void const *content, size_t content_size);
 
 #endif
