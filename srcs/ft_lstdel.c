982b9e2a544f49d4bfbf1e501815b65ae84be963
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-15 17:21:27 -0800

    Add ft_lstdel

---
+++ srcs/ft_lstdel.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdel.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/15 17:12:36 by drosa-ta          #+#    #+#             */
+/*   Updated: 2017/12/15 17:12:37 by drosa-ta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+void 	ft_lstdel(t_list **alst, void (*del)(void *,size_t))
+{
+	t_list *next_node;
+
+	while (*alst)
+	{
+		next_node = (*alst)->next;
+		del((*alst)->content, (*alst)->content_size);
+		free(*alst);
+		*alst = NULL;
+		*alst = next_node;
+	}
+}
+
