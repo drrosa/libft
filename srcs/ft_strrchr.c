fb8cf0434eb0201f9a49f9a97cfbd1cbecfaab7c
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-16 16:25:28 -0800

    ft_strrchr: Fix bug i not reaching index 0.

--- srcs/ft_strrchr.c
+++ srcs/ft_strrchr.c
@@ -17,13 +17,11 @@ char	*ft_strrchr(const char *s, int c)
 	int i;
 
 	i = ft_strlen(s);
-	while (i > 0)
+	while (i >= 0)
 	{
 		if (s[i] == c)
 			return ((char *)s + i);
 		i--;
 	}
-	if (c == '\0')
-		return ((char *)s + i);
 	return (NULL);
 }
