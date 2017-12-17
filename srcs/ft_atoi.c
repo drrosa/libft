cdb99d8324f343ebd0a5dcad88504a1153ca24dd
David Rosa Tamsen <rosatamsen@gmail.com>
2017-12-17 00:26:59 -0800

    norm: Fix norminette issues.

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,27 +6,27 @@
 /*   By: drosa-ta <drosa-ta@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 18:10:42 by drosa-ta          #+#    #+#             */
-/*   Updated: 2017/12/05 21:40:48 by drosa-ta         ###   ########.fr       */
+/*   Updated: 2017/12/17 00:25:26 by drosa-ta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int     ft_atoi(const char *str)
+int		ft_atoi(const char *str)
 {
-    int     result;
-    int     sign;
-    int     i;
+	int		result;
+	int		sign;
+	int		i;
 
-    result = 0;
-    sign = 1;
-    i = 0;
-    while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' ||
-           str[i] == '\v' || str[i] == '\f' || str[i] == ' ')
-        i++;
-    if (str[i] == '-')
-        sign = -1;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    while (str[i] && str[i] >= '0' && str[i] <= '9')
-        result = result * 10 + str[i++] - '0';
-    return (result * sign);
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (str[i] == '\n' || str[i] == '\t' || str[i] == '\r' ||
+			str[i] == '\v' || str[i] == '\f' || str[i] == ' ')
+		i++;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	while (str[i] && str[i] >= '0' && str[i] <= '9')
+		result = result * 10 + str[i++] - '0';
+	return (result * sign);
 }
